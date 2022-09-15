fe8da54c562140cd9f750bff33ac56386dce39a9
seeunpark <senasoon0@gmail.com>
2022-09-15 17:22:42 +0900

    chore: 수정

--- src/pages/LecturePage.jsx
+++ src/pages/LecturePage.jsx
@@ -140,6 +140,7 @@ const LecturePage = () => {
         entireScore={entireScore}
         onEditHandler={onEditHandler}
         onDeleteHandler={onDeleteHandler}
+        lectureId={lecture.id}
       />
     </>
   );
